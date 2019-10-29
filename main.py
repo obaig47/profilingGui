cee04cf3907171334fa60dc70c10ccb19b71db03
Omar Baig <omar.baig94@gmail.com>
2019-10-29 12:28:31 -0400

    Fixed all essential functionality, ready for deployment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
+import sys, os
 from PyQt4 import QtGui, QtCore  # QtCore is for event handling
-from PyQt4.QtGui import QFileDialog
+from PyQt4.QtGui import QFileDialog, QMessageBox
 
 
 class Widget(QtGui.QWidget):
@@ -8,54 +8,42 @@ class Widget(QtGui.QWidget):
     def __init__(self):
         super(Widget, self).__init__()
 
+        # Static variables
+        self.destinationDir = ""
+        self.fileName = ""
+        self.completed = 0
+        self.load_btn = QtGui.QPushButton('Load Data File', self)
+        self.profile_btn = QtGui.QPushButton("Create Profile", self)
+        self.progress = QtGui.QProgressBar(self)
+
         self.initUI()
 
     def initUI(self):
 
         # Main Window
-        # self.resize(320, 240)
         self.setGeometry(0, 0, 320, 175)
         self.setWindowTitle('Data Profiler')
         self.setWindowIcon(QtGui.QIcon('server.png'))
         self.center()
 
-        # Toolbar
-        # toolBar = self.addToolBar('Main Toolbar')
-        # extractAction = QtGui.QAction(QtGui.QIcon('open_xlsx.png'), 'Load Excel File', self)
-        # extractAction.triggered.connect(self.close_application)
-        # toolBar.addAction(extractAction)
-
-        # QtGui.QWidget.__init__(self)
-        self.excel_btn = QtGui.QPushButton('Load Excel File', self)
-        self.excel_btn.clicked.connect(self.fileBrowse)
-        self.excel_btn.setIcon(QtGui.QIcon('open_xlsx.png'))
-        # self.button.setIconSize(QtCore.QSize(24, 24))
-        # layout = QtGui.QVBoxLayout(self)
-        # layout.addWidget(self.button)
-
-        self.csv_btn = QtGui.QPushButton('Load CSV File', self)
-        self.csv_btn.clicked.connect(self.fileBrowse)
-        self.csv_btn.setIcon(QtGui.QIcon('open_csv.png'))
-        self.csv_btn.move(100, 0)
-
-        self.csv_btn = QtGui.QPushButton('Profile Destination', self)
-        self.csv_btn.clicked.connect(self.fileBrowse)
-        self.csv_btn.setIcon(QtGui.QIcon('save.png'))
-        self.csv_btn.move(200, 0)
+        # Load Data File Button
+        self.load_btn.setGeometry(0, 0, 320, 30)
+        self.load_btn.clicked.connect(self.fileBrowse)
+        self.load_btn.setIcon(QtGui.QIcon('open_xlsx.png'))
+        # self.excel_btn.setIconSize(QtCore.QSize(24, 24))
 
         # Progress bar
-        self.progress = QtGui.QProgressBar(self)
         self.progress.setGeometry(55, 70, 250, 20)
 
         # Convert File Button
-        self.btn = QtGui.QPushButton("Create Profile", self)
-        self.btn.resize(self.btn.sizeHint())
-        self.btn.clicked.connect(self.fileBrowse)
-        self.btn.clicked.connect(self.convert)
-        self.btn.move(125, 40)
+        self.profile_btn.setGeometry(0, 30, 320, 30)
+        # self.profile_btn.resize(self.profile_btn.sizeHint())
+        self.profile_btn.clicked.connect(self.convert)
+        self.profile_btn.setIcon(QtGui.QIcon('effort.png'))
+        # self.profile_btn.move(125, 40)
 
         # Opened files being stored here
-        self.csv = []
+        # self.csv = []
 
         self.show()
 
@@ -67,52 +55,78 @@ class Widget(QtGui.QWidget):
         self.move(frameGm.topLeft())
 
     def close_application(self):
-        choice = QtGui.QMessageBox.question(self, 'Exit',
+        choice = QtGui.QMessageBox.question(None, 'Exit',
                                             "Are you sure you want to quit?",
                                             QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
 
         if choice == QtGui.QMessageBox.Yes:
-            print("Extracting Naaaaaoooww!!!")
+            print("Selected Yes to quit from QMessageBox")
             sys.exit()
         else:
             pass
 
     def fileBrowse(self):
-        if len(self.csv) < 2:
-            filePath = QtGui.QFileDialog.getOpenFileName(self,
-                                                         '',
-                                                         "Desktop",
-                                                         '*.csv')
+        # Load File Dialog
+        fileName = QFileDialog.getOpenFileName(None,
+                                               "Select Data File", "Desktop", "Data Files (*.csv *.xlsx *.xls)")
 
-            if filePath != "" and not filePath in self.csv:
-                self.csv.append(filePath)
-        if len(self.csv) == 2:
-            self.process()
+        self.fileName = fileName
+        # print(self.fileName)
+
+        # Select Destination Directory Dialog
+        directory = QFileDialog.getExistingDirectory(None, "Select Destination Directory")
+
+        self.destinationDir = directory
+        # print(self.destinationDir)
 
     def convert(self):
-        self.completed = 0
+        # Progress Bar Update
+        while self.completed < 10:
+            self.completed += 0.0001
+            self.progress.setValue(self.completed)
+
+        import pandas as pd
+
+        # Extracting file type, calling appropriate read function
+        fileType = self.fileName.split('.')[1]
+
+        if fileType == 'csv':
+            df = pd.read_csv(self.fileName)
+            print(df.head())
+        elif fileType == 'xlsx' or fileType == 'xls':
+            df = pd.read_excel(self.fileName)
+            print(df.head())
+        else:
+            pass
+
+        while self.completed < 50:
+            self.completed += 0.0001
+            self.progress.setValue(self.completed)
+
+        import pandas_profiling as pp
+
+        # Getting file stem to use for the new html file being created
+        fileStemArray = self.fileName.split('/')
+        fileStem = fileStemArray[len(fileStemArray) - 1].split('.')[0]
+
+        # Creating profile using pandas_profiling
+        profile = pp.ProfileReport(df)
+
+        while self.completed < 90:
+            self.completed += 0.0001
+            self.progress.setValue(self.completed)
+
+        export_file = self.destinationDir + '\\Profile_' + fileStem + '.html'
+        # print(export_file)
+        profile.to_file(output_file=export_file)
 
         while self.completed < 100:
             self.completed += 0.0001
             self.progress.setValue(self.completed)
 
-        import pandas as pd
-        from pandas import DataFrame
-
-        df1 = pd.read_csv(self.csv[0])
-        df2 = pd.read_csv(self.csv[1])
-
-        df3 = df1.loc[:, ['a_column', 'b_column']]
-
-        df3[""] = ""
-
-        df4 = df2.loc[:, ['c_column', 'd_column', 'e_column']]
-
-        new = pd.concat([df3, df4], axis=1)
-
-        new.index = new.index + 1
-
-        new.to_csv('csv2.csv')
+        # Open the newly created file
+        import webbrowser
+        webbrowser.open('file://' + os.path.realpath(export_file))
 
 
 def main():
